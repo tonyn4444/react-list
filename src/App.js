2a0571f9692a35075d28e79454d5df51dbc14ae1
Tony Nguyen <tntuyen@ucdavis.edu>
2017-03-10 21:19:45 -0800

    add social-media buttons

--- src/App.js
+++ src/App.js
@@ -2,15 +2,28 @@ import React, { Component } from 'react';
 import portrait from './portfolio_pictures/portrait.jpg';
 import './App.css';
 import ProjectList from './containers/ProjectList';
+import { SocialIcon } from 'react-social-icons';
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <div className="App-header">
-          <img src={portrait} className="App-logo" alt="logo" />
-          <h2>Hi, I'm Tony</h2>
-          <h3>Here are some projects I've been working on</h3>
+          <div id="social-icons">
+            <SocialIcon url="https://www.github.com/tonyn4444" />
+            <SocialIcon url="https://www.linkedin.com/in/tony-t-nguyen" />
+            <SocialIcon url="https://www.twitter.com/tonytweetertwit" />
+          </div>
+
+          <div id="intro">
+            <img src={portrait} className="App-logo" alt="logo" />
+            <h2>Hi, I'm Tony</h2>
+            <h3>Here are some projects I've been working on</h3>
+          </div>
+
+         
+
         </div>
         <p className="App-intro">
         </p>
--- src/reducers/projects_reducer.js
+++ src/reducers/projects_reducer.js
@@ -2,12 +2,16 @@ import todo_list_thumbnail from '../portfolio_pictures/thumbnail_todo_app.png';
 import weather_forecast_thumbnail from '../portfolio_pictures/thumbnail_weather_forecast_app.png';
 import youtube_search_thumbnail from '../portfolio_pictures/thumbnail_youtube_search.png';
 import blog_thumbnail from '../portfolio_pictures/thumbnail_blog_app.png';
+import barID from '../portfolio_pictures/barID.png';
+import financeGo from '../portfolio_pictures/finance-go.png';
 
 export default function() {
 	return [
-		{ name: 'To Do List', thumbnail_image: todo_list_thumbnail },
+		{ name: 'Finance-Go', thumbnail_image: financeGo },
+		{ name: 'BarID', thumbnail_image: barID },
 		{ name: 'Weather Forecast', thumbnail_image: weather_forecast_thumbnail },
+		{ name: 'Blog', thumbnail_image: blog_thumbnail },
 		{ name: 'Youtube Search', thumbnail_image: youtube_search_thumbnail },
-		{ name: 'Blog', thumbnail_image: blog_thumbnail }
+		{ name: 'To Do List', thumbnail_image: todo_list_thumbnail }
 	];
 }
