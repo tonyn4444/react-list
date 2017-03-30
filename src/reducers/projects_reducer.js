6653c66b695abab4d7495828e112e2a7655ad043
Tony Nguyen <tntuyen@ucdavis.edu>
2017-03-29 18:39:47 -0700

    add character voting app

--- src/reducers/projects_reducer.js
+++ src/reducers/projects_reducer.js
@@ -8,11 +8,13 @@ import youtube_search_full from '../portfolio_pictures/youtube_search.png'
 import weather_forecast_full from '../portfolio_pictures/weather_forecast_full.png';
 import blog_full from '../portfolio_pictures/blog_full.png';
 import barID_full from '../portfolio_pictures/barID_full.png';
+import newedenfaces from '../portfolio_pictures/newedenfaces.png';
 
 export default function() {
 	return [
 		{ name: 'Finance-Go', thumbnail_image: financeGo, about: about_financeGO, stack: financeGO_stack },
 		{ name: 'Weather Forecast', thumbnail_image: weather_forecast_full, about: about_weather_forecast, stack: weather_forecast_stack },
+		{ name: 'EVE Character Voting', thumbnail_image: newedenfaces, about: about_newedenfaces, stack: newedenfaces_stack },
 		{ name: 'Youtube Search', thumbnail_image: youtube_search_full, about: about_youtube_search, stack: youtube_search_stack },
 		{ name: 'BarID', thumbnail_image: barID_full, about: about_barID, stack: barID_stack },
 		{ name: 'Blog', thumbnail_image: blog_full, about: about_blog, stack: blog_stack }
@@ -20,12 +22,14 @@ export default function() {
 	];
 }
 
+const newedenfaces_stack = "React + Flux - Utilizes MongoDB to save and retrieve data, Socket.IO to track online visitors in real time, and Node.js to build a REST API."
 const barID_stack = "React Native – Redux – Firebase API – iBeacon (IOS Location Architecture)"
 const financeGO_stack = "Ruby on Rails - Devise - Postgresql - YahooFinance API."
 const blog_stack = "React/Redux - React-Router - ReduxForm - Axios"
 const youtube_search_stack = "React/Redux - Axios - YouTube API"
 const weather_forecast_stack = "React-Sparklines - OpenWeatherMap API - React/Redux - Axios - Lodash"
 
+const about_newedenfaces = "Single-page application that retrieves data from a popular online multiplayer EVE Online, and allows plays to vote for their favorite player's avatar."
 const about_barID = "An application that makes communication and purchasing of drinks easier between bars and their prospective clients. "
 const about_financeGO = "This is an application that allows users to easily keep track of company stocks in the market. Users can also search for, add, and view profiles of their friends on the site. "
 const about_blog = "Blog Application created using React/Redux. Network requests made from separate API to store and retrieve data for posts."
